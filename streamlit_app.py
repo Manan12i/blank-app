6257954ec89fe016e354538dba518d00178d7162
Manan12i <manan.darshan12a@gmail.com>
2024-11-23 01:43:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="My Webpage",page_icon=":tada",layout="wide")
 with st.container():
    st.title("Hello Manan!! :wave:")
    st.header("Computer Science Project")
